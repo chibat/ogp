206b71ddbcf22f9623a9daac4853034e55f0be5f
Tomofumi Chiba <tomofumi.chiba@gmail.com>
2021-11-13 21:55:48 +0900

    fix

--- app.tsx
+++ app.tsx
@@ -99,7 +99,7 @@ function SpecifyUrl() {
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous"></link>
       </head>
       <body>
-        <div class="container-fluid">
+        <div class="container">
           <h1>Open Graph Preview</h1>
           <div class="alert alert-primary" role="alert">
             Specify "url" Parameter
