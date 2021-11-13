2205bfcc38140eab3bb8a11d6a917f5f786faa54
Tomofumi Chiba <tomofumi.chiba@gmail.com>
2021-11-13 22:39:15 +0900

    fix

--- app.tsx
+++ app.tsx
@@ -68,6 +68,7 @@ function App({ map, url }: { map: Map<string, string>, url: string }) {
     <html>
       <head>
         <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous"></link>
       </head>
       <body>
@@ -95,6 +96,7 @@ function SpecifyUrl() {
     <html>
       <head>
         <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <title>Open Graph Preview</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous"></link>
       </head>
