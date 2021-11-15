91af23b8d1b5df74e852cd2c24501143cf7d8d52
Tomofumi Chiba <tomofumi.chiba@gmail.com>
2021-11-15 21:06:29 +0900

    fix

--- app.tsx
+++ app.tsx
@@ -113,10 +113,9 @@ function App({ map, url, size }: { map: Map<string, string>, url: string, size: 
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous"></link>
       </head>
       <body>
-        {size === "small" &&
+        {size === "small" ?
           <Small title={title} description={description} url={url} site={site} image={image} />
-        }
-        {size !== "small" &&
+          :
           <Large title={title} description={description} url={url} site={site} image={image} />
         }
       </body>
